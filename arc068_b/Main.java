8b254d2b10197e148cc5d8b03fc69e2ad50075ca
Kai Wang <charles.wangkai@gmail.com>
2020-04-28 14:48:09 +0800

    Card Eater

---
+++ arc068_b/Main.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < A.length; ++i) {
+            A[i] = sc.nextInt();
+        }
+
+        System.out.println(solve(A));
+
+        sc.close();
+    }
+
+    static int solve(int[] A) {
+        Map<Integer, Integer> valueToCount = new HashMap<>();
+        for (int value : A) {
+            valueToCount.put(value, valueToCount.getOrDefault(value, 0) + 1);
+        }
+
+        return A.length - (valueToCount.values().stream().mapToInt(x -> x - 1).sum() + 1) / 2 * 2;
+    }
+}
