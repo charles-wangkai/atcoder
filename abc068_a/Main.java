ee44a127b6f52132a71a0399e4c4af05e0d4e878
Kai Wang <charles.wangkai@gmail.com>
2020-05-12 07:05:35 +0800

    ABCxxx

---
+++ abc068_a/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        System.out.println(solve(N));
+
+        sc.close();
+    }
+
+    static String solve(int N) {
+        return String.format("ABC%d", N);
+    }
+}
