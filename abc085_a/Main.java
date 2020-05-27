3d91791b80dbbdd029e043f6214d9e0dd0d9c198
Kai Wang <charles.wangkai@gmail.com>
2020-05-28 00:30:12 +0800

    Already 2018

---
+++ abc085_a/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String S = sc.next();
+
+        System.out.println(solve(S));
+
+        sc.close();
+    }
+
+    static String solve(String S) {
+        return S.replaceAll("2017", "2018");
+    }
+}
