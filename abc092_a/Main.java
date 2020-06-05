5882939cb093137a082168a4564dc84d73ffcb46
Kai Wang <charles.wangkai@gmail.com>
2020-06-06 01:02:55 +0800

    Traveling Budget

---
+++ abc092_a/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int D = sc.nextInt();
+
+        System.out.println(solve(A, B, C, D));
+
+        sc.close();
+    }
+
+    static int solve(int A, int B, int C, int D) {
+        return Math.min(A, B) + Math.min(C, D);
+    }
+}
