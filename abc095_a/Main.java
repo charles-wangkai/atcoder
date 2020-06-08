538a55abb7c5bcf6c081bb9576d45dd58c85cbf3
Kai Wang <charles.wangkai@gmail.com>
2020-06-09 00:22:18 +0800

    Something on It

---
+++ abc095_a/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String S = sc.next();
+
+        System.out.println(solve(S));
+
+        sc.close();
+    }
+
+    static int solve(String S) {
+        return 700 + S.replace("x", "").length() * 100;
+    }
+}
