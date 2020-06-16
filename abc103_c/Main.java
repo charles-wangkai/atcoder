9e07035099c43e6d42602c4b03717f91d029f93f
Kai Wang <charles.wangkai@gmail.com>
2020-06-16 23:58:54 +0800

    Modulo Summation

---
+++ abc103_c/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] a = new int[N];
+        for (int i = 0; i < a.length; ++i) {
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println(solve(a));
+
+        sc.close();
+    }
+
+    static int solve(int[] a) {
+        return Arrays.stream(a).map(x -> x - 1).sum();
+    }
+}
