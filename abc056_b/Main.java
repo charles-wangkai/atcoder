6bfa8ae60f43b68fb77c28896f4bea64c70ef0ff
Kai Wang <charles.wangkai@gmail.com>
2026-02-26 16:20:07 +0800

    NarrowRectanglesEasy

---
+++ abc056_b/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int W = sc.nextInt();
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println(solve(W, a, b));
+
+    sc.close();
+  }
+
+  static int solve(int W, int a, int b) {
+    if (b > a + W) {
+      return b - (a + W);
+    }
+    if (a > b + W) {
+      return a - (b + W);
+    }
+
+    return 0;
+  }
+}
