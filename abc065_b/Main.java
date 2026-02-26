be8a70b72d94f1e886f7968a2fcf5f8a18545c6c
Kai Wang <charles.wangkai@gmail.com>
2026-02-26 19:53:56 +0800

    Trained?

---
+++ abc065_b/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] a = new int[N];
+    for (int i = 0; i < a.length; ++i) {
+      a[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(a));
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    boolean[] visited = new boolean[a.length];
+    visited[0] = true;
+
+    int index = 0;
+    for (int step = 1; ; ++step) {
+      index = a[index] - 1;
+      if (index == 1) {
+        return step;
+      }
+      if (visited[index]) {
+        return -1;
+      }
+
+      visited[index] = true;
+    }
+  }
+}
