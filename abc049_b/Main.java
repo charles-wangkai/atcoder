5f03f67c13d3fdc4472926ef219ed1cc6519aec1
Kai Wang <charles.wangkai@gmail.com>
2026-02-26 15:09:06 +0800

    Thin

---
+++ abc049_b/Main.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int H = sc.nextInt();
+    int W = sc.nextInt();
+    char[][] C = new char[H][W];
+    for (int r = 0; r < H; ++r) {
+      String line = sc.next();
+      for (int c = 0; c < W; ++c) {
+        C[r][c] = line.charAt(c);
+      }
+    }
+
+    System.out.println(solve(C));
+
+    sc.close();
+  }
+
+  static String solve(char[][] C) {
+    return Arrays.stream(C)
+        .flatMap(line -> Stream.of(line, line))
+        .map(String::valueOf)
+        .collect(Collectors.joining("\n"));
+  }
+}
