0d9ac9a8e59ff2a0e1032eff795cd4e3ed17a5fe
Kai Wang <charles.wangkai@gmail.com>
2026-02-26 16:51:33 +0800

    Three-letter acronym

---
+++ abc059_a/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String[] s = new String[3];
+    for (int i = 0; i < s.length; ++i) {
+      s[i] = sc.next();
+    }
+
+    System.out.println(solve(s));
+
+    sc.close();
+  }
+
+  static String solve(String[] s) {
+    return Arrays.stream(s)
+        .map(si -> si.charAt(0))
+        .map(Character::toUpperCase)
+        .map(String::valueOf)
+        .collect(Collectors.joining());
+  }
+}
