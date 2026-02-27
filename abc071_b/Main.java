2c925aa5c4392586f142f1bf920476b9015d091c
Kai Wang <charles.wangkai@gmail.com>
2026-02-27 13:50:55 +0800

    Not Found

---
+++ abc071_b/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String S = sc.next();
+
+    System.out.println(solve(S));
+
+    sc.close();
+  }
+
+  static String solve(String S) {
+    Set<Integer> seen = S.chars().boxed().collect(Collectors.toSet());
+
+    return IntStream.rangeClosed('a', 'z')
+        .filter(c -> !seen.contains(c))
+        .mapToObj(c -> (char) c)
+        .map(String::valueOf)
+        .findFirst()
+        .orElse("None");
+  }
+}
