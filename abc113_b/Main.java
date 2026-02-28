49bc7598ab3bfe043a167c88373821c183a0e710
Kai Wang <charles.wangkai@gmail.com>
2026-02-28 13:59:48 +0800

    Palace

---
+++ abc113_b/Main.java
@@ -0,0 +1,29 @@
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int T = sc.nextInt();
+    int A = sc.nextInt();
+    int[] H = new int[N];
+    for (int i = 0; i < H.length; ++i) {
+      H[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(H, T, A));
+
+    sc.close();
+  }
+
+  static int solve(int[] H, int T, int A) {
+    return IntStream.range(0, H.length)
+            .boxed()
+            .min(Comparator.comparing(i -> Math.abs(1000 * T - 6 * H[i] - 1000 * A)))
+            .get()
+        + 1;
+  }
+}
