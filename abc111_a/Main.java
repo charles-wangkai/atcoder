d4251ba5e402ffe388b1da548972242bf3ba0c69
Kai Wang <charles.wangkai@gmail.com>
2026-02-28 13:35:14 +0800

    AtCoder Beginner Contest 999

---
+++ abc111_a/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    System.out.println(solve(n));
+
+    sc.close();
+  }
+
+  static String solve(int n) {
+    return String.valueOf(n)
+        .chars()
+        .mapToObj(c -> (char) ('1' + '9' - c))
+        .map(String::valueOf)
+        .collect(Collectors.joining());
+  }
+}
