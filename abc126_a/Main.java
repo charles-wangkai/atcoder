919fccaeca22dda8e9cc7ab3b5251a9e682fcbac
Kai Wang <charles.wangkai@gmail.com>
2026-02-28 17:11:19 +0800

    Changing a Character

---
+++ abc126_a/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    sc.nextInt();
+    int K = sc.nextInt();
+    String S = sc.next();
+
+    System.out.println(solve(S, K));
+
+    sc.close();
+  }
+
+  static String solve(String S, int K) {
+    return IntStream.range(0, S.length())
+        .mapToObj(i -> (i == K - 1) ? Character.toLowerCase(S.charAt(i)) : S.charAt(i))
+        .map(String::valueOf)
+        .collect(Collectors.joining());
+  }
+}
