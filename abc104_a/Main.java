af34c2428683a39fb330ac46f051a5b0dd761e6e
Kai Wang <charles.wangkai@gmail.com>
2026-02-28 11:15:13 +0800

    Rated for Me

---
+++ abc104_a/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int R = sc.nextInt();
+
+    System.out.println(solve(R));
+
+    sc.close();
+  }
+
+  static String solve(int R) {
+    if (R < 1200) {
+      return "ABC";
+    }
+    if (R < 2800) {
+      return "ARC";
+    }
+
+    return "AGC";
+  }
+}
