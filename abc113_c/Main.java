72375b3abb21615efd48f48cb27ab490127db696
Kai Wang <charles.wangkai@gmail.com>
2026-02-28 14:06:47 +0800

    ID

---
+++ abc113_c/Main.java
@@ -0,0 +1,39 @@
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int M = sc.nextInt();
+    int[] P = new int[M];
+    int[] Y = new int[M];
+    for (int i = 0; i < M; ++i) {
+      P[i] = sc.nextInt();
+      Y[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(N, P, Y));
+
+    sc.close();
+  }
+
+  static String solve(int N, int[] P, int[] Y) {
+    String[] result = new String[P.length];
+    int[] counts = new int[N + 1];
+    int[] sortedIndices =
+        IntStream.range(0, P.length)
+            .boxed()
+            .sorted(Comparator.comparing(i -> Y[i]))
+            .mapToInt(Integer::intValue)
+            .toArray();
+    for (int index : sortedIndices) {
+      ++counts[P[index]];
+      result[index] = "%06d%06d".formatted(P[index], counts[P[index]]);
+    }
+
+    return String.join("\n", result);
+  }
+}
