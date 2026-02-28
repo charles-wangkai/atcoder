daba91a25d67b3a4a55e0250f803e9dc18bb5a6b
Kai Wang <charles.wangkai@gmail.com>
2026-02-28 14:28:03 +0800

    Christmas Eve Eve Eve

---
+++ abc115_a/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int D = sc.nextInt();
+
+    System.out.println(solve(D));
+
+    sc.close();
+  }
+
+  static String solve(int D) {
+    if (D == 25) {
+      return "Christmas";
+    }
+    if (D == 24) {
+      return "Christmas Eve";
+    }
+    if (D == 23) {
+      return "Christmas Eve Eve";
+    }
+
+    return "Christmas Eve Eve Eve";
+  }
+}
