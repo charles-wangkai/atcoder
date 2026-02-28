5a1cf7b89929d079b0d89ddaf2a0e5def0e741be
Kai Wang <charles.wangkai@gmail.com>
2026-02-28 23:19:14 +0800

    Bite Eating

---
+++ abc131_b/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int L = sc.nextInt();
+
+    System.out.println(solve(N, L));
+
+    sc.close();
+  }
+
+  static int solve(int N, int L) {
+    int[] flavors = IntStream.range(0, N).map(i -> L + i).toArray();
+
+    return Arrays.stream(flavors).sum()
+        - Arrays.stream(flavors).boxed().min(Comparator.comparing(Math::abs)).get();
+  }
+}
