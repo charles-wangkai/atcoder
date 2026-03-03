ef0fac4dd83604dc1542e925bf4a37280d9d0fc8
Kai Wang <charles.wangkai@gmail.com>
2026-03-03 10:05:10 +0800

    AtCoDeer and Election Report

---
+++ arc062_a/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] T = new int[N];
+    int[] A = new int[N];
+    for (int i = 0; i < N; ++i) {
+      T[i] = sc.nextInt();
+      A[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(T, A));
+
+    sc.close();
+  }
+
+  static long solve(int[] T, int[] A) {
+    long t = 1;
+    long a = 1;
+    for (int i = 0; i < T.length; ++i) {
+      long factor = Math.max(Math.ceilDiv(t, T[i]), Math.ceilDiv(a, A[i]));
+
+      t = T[i] * factor;
+      a = A[i] * factor;
+    }
+
+    return t + a;
+  }
+}
