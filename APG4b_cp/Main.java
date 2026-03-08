43b8cdbadc8cb3f804fdca622d6ec1a75c5e522b
Kai Wang <charles.wangkai@gmail.com>
2026-03-08 11:43:10 +0800

    1.07

--- APG4b_cp/Main.java
+++ APG4b_cp/Main.java
@@ -3,7 +3,33 @@ public class Main {
     System.out.println(solve());
   }
 
+  @SuppressWarnings("unused")
   static String solve() {
-    return "AtCoder";
+    boolean a = true;
+    boolean b = false;
+    boolean c = true;
+
+    StringBuilder result = new StringBuilder();
+    if (a) {
+      result.append("At");
+    } else {
+      result.append("Yo");
+    }
+
+    if (!a && b) {
+      result.append("Bo");
+    } else if (!b || c) {
+      result.append("Co");
+    }
+
+    if (a && b && c) {
+      result.append("foo!");
+    } else if (true && false) {
+      result.append("yeah!");
+    } else if (!a || c) {
+      result.append("der");
+    }
+
+    return result.toString();
   }
 }
