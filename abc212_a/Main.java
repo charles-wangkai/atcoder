c5babf127bfa472ccc2d57a90e47f54e98f303bf
Kai Wang <charles.wangkai@gmail.com>
2026-03-10 20:59:42 +0800

    Alloy

---
+++ abc212_a/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+
+    System.out.println(solve(A, B));
+
+    sc.close();
+  }
+
+  static String solve(int A, int B) {
+    if (B == 0) {
+      return "Gold";
+    }
+    if (A == 0) {
+      return "Silver";
+    }
+
+    return "Alloy";
+  }
+}
