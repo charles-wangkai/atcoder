d2e75fded3f6dfbdc81013e66fac6eee42f8b40c
Kai Wang <charles.wangkai@gmail.com>
2026-03-10 10:39:48 +0800

    1.13

---
+++ APG4b_cj/Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] A = new int[N];
+    for (int i = 0; i < A.length; ++i) {
+      A[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(A));
+
+    sc.close();
+  }
+
+  static String solve(int[] A) {
+    int avg = Arrays.stream(A).sum() / A.length;
+
+    return Arrays.stream(A)
+        .map(Ai -> Math.abs(Ai - avg))
+        .mapToObj(String::valueOf)
+        .collect(Collectors.joining("\n"));
+  }
+}
