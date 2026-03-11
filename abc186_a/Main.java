4f7d4c502487048c13c5758d108443a457e6f711
Kai Wang <charles.wangkai@gmail.com>
2026-03-11 12:58:54 +0800

    Brick

---
+++ abc186_a/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int W = sc.nextInt();
+
+    System.out.println(solve(N, W));
+
+    sc.close();
+  }
+
+  static int solve(int N, int W) {
+    return N / W;
+  }
+}
