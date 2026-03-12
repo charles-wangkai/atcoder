c3b9cf3316b0560dc218ff71b9ece77a0a1290f9
Kai Wang <charles.wangkai@gmail.com>
2026-03-12 18:10:24 +0800

    Find Multiple

---
+++ abc220_a/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+    int C = sc.nextInt();
+
+    System.out.println(solve(A, B, C));
+
+    sc.close();
+  }
+
+  static int solve(int A, int B, int C) {
+    return IntStream.rangeClosed(A, B).filter(x -> x % C == 0).findAny().orElse(-1);
+  }
+}
