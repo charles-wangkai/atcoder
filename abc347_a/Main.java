28e5293c39ee5965b6c614a6eabca4f3319069d5
Kai Wang <charles.wangkai@gmail.com>
2026-03-13 17:26:57 +0800

    Divisible

---
+++ abc347_a/Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int K = sc.nextInt();
+    int[] A = new int[N];
+    for (int i = 0; i < A.length; ++i) {
+      A[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(A, K));
+
+    sc.close();
+  }
+
+  static String solve(int[] A, int K) {
+    return Arrays.stream(A)
+        .filter(x -> x % K == 0)
+        .map(x -> x / K)
+        .mapToObj(String::valueOf)
+        .collect(Collectors.joining(" "));
+  }
+}
