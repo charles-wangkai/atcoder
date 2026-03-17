d4bfab32c1d5620117e27efd9efc6fddedd5d282
Kai Wang <charles.wangkai@gmail.com>
2026-03-17 15:47:50 +0800

    Uneven Numbers

---
+++ abc136_b/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+
+    System.out.println(solve(N));
+
+    sc.close();
+  }
+
+  static int solve(int N) {
+    return (int)
+        IntStream.rangeClosed(1, N).filter(x -> String.valueOf(x).length() % 2 == 1).count();
+  }
+}
