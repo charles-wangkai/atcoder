41398f6c497cd6ee34950d077dfa1643035a92b5
Kai Wang <charles.wangkai@gmail.com>
2026-03-18 10:47:34 +0800

    Same

---
+++ abc324_a/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] A = new int[N];
+    for (int i = 0; i < A.length; ++i) {
+      A[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(A) ? "Yes" : "No");
+
+    sc.close();
+  }
+
+  static boolean solve(int[] A) {
+    return Arrays.stream(A).distinct().count() == 1;
+  }
+}
