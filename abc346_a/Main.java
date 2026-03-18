f28f7041d13e60cb12199efcdc1ea57c4e8a10a2
Kai Wang <charles.wangkai@gmail.com>
2026-03-18 22:28:12 +0800

    Adjacent Product

---
+++ abc346_a/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] A = new int[N];
+    for (int i = 0; i < A.length; ++i) {
+      A[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(A));
+
+    sc.close();
+  }
+
+  static String solve(int[] A) {
+    return IntStream.range(0, A.length - 1)
+        .map(i -> A[i] * A[i + 1])
+        .mapToObj(String::valueOf)
+        .collect(Collectors.joining(" "));
+  }
+}
