5604c327e8ac88cf42b5420763dea0ebadf65665
Kai Wang <charles.wangkai@gmail.com>
2026-03-18 17:25:41 +0800

    Leap Year

---
+++ abc365_a/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int Y = sc.nextInt();
+
+    System.out.println(solve(Y));
+
+    sc.close();
+  }
+
+  static int solve(int Y) {
+    return (Y % 4 == 0 && (Y % 100 != 0 || Y % 400 == 0)) ? 366 : 365;
+  }
+}
