e9fa625e5cdd0f79cd369490e6d9c469c83057eb
Kai Wang <charles.wangkai@gmail.com>
2026-03-19 16:34:24 +0800

    2UP3DOWN

---
+++ abc326_a/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int X = sc.nextInt();
+    int Y = sc.nextInt();
+
+    System.out.println(solve(X, Y) ? "Yes" : "No");
+
+    sc.close();
+  }
+
+  static boolean solve(int X, int Y) {
+    return (Y >= X && Y - X <= 2) || (Y < X && X - Y <= 3);
+  }
+}
