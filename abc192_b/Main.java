91265deb6c0b98d9b6905ec544d095c6a215b2cc
Kai Wang <charles.wangkai@gmail.com>
2026-03-19 18:16:25 +0800

    uNrEaDaBlE sTrInG

---
+++ abc192_b/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String S = sc.next();
+
+    System.out.println(solve(S) ? "Yes" : "No");
+
+    sc.close();
+  }
+
+  static boolean solve(String S) {
+    return IntStream.range(0, S.length())
+        .allMatch(
+            i ->
+                (i % 2 == 0)
+                    ? Character.isLowerCase(S.charAt(i))
+                    : Character.isUpperCase(S.charAt(i)));
+  }
+}
