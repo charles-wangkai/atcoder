79ad9e2845fd3e5a9825ff344570ba03e468b46f
Kai Wang <charles.wangkai@gmail.com>
2026-03-19 18:19:21 +0800

    First ABC

---
+++ abc311_a/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    sc.nextInt();
+    String S = sc.next();
+
+    System.out.println(solve(S));
+
+    sc.close();
+  }
+
+  static int solve(String S) {
+    return IntStream.of(S.indexOf('A'), S.indexOf('B'), S.indexOf('C')).max().getAsInt() + 1;
+  }
+}
