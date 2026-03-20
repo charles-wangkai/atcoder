3329788809b216a07997bf5bc965d39195354de2
Kai Wang <charles.wangkai@gmail.com>
2026-03-20 17:23:27 +0800

    Count Takahashi

---
+++ abc359_a/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    String[] S = new String[N];
+    for (int i = 0; i < S.length; ++i) {
+      S[i] = sc.next();
+    }
+
+    System.out.println(solve(S));
+
+    sc.close();
+  }
+
+  static int solve(String[] S) {
+    return (int) Arrays.stream(S).filter(Si -> Si.equals("Takahashi")).count();
+  }
+}
