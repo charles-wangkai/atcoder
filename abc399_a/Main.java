16c137505e01581ebaa6ac61724f860365868e6e
Kai Wang <charles.wangkai@gmail.com>
2026-03-20 16:53:06 +0800

    Hamming Distance

---
+++ abc399_a/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    sc.nextInt();
+    String S = sc.next();
+    String T = sc.next();
+
+    System.out.println(solve(S, T));
+
+    sc.close();
+  }
+
+  static int solve(String S, String T) {
+    return (int) IntStream.range(0, S.length()).filter(i -> S.charAt(i) != T.charAt(i)).count();
+  }
+}
