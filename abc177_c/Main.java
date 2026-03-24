7bab7838c8046ded7384658ba6b584888078441d
Kai Wang <charles.wangkai@gmail.com>
2026-03-24 11:07:01 +0800

    Sum of product of pairs

---
+++ abc177_c/Main.java
@@ -0,0 +1,69 @@
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  static final ModInt MOD_INT = new ModInt(1_000_000_007);
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] A = new int[N];
+    for (int i = 0; i < A.length; ++i) {
+      A[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(A));
+
+    sc.close();
+  }
+
+  static int solve(int[] A) {
+    int result = 0;
+    int suffixSum = Arrays.stream(A).reduce(0, MOD_INT::addMod);
+    for (int Ai : A) {
+      suffixSum = MOD_INT.addMod(suffixSum, -Ai);
+      result = MOD_INT.addMod(result, MOD_INT.multiplyMod(Ai, suffixSum));
+    }
+
+    return result;
+  }
+}
+
+class ModInt {
+  int modulus;
+
+  ModInt(int modulus) {
+    this.modulus = modulus;
+  }
+
+  int mod(long x) {
+    return Math.floorMod(x, modulus);
+  }
+
+  int modInv(int x) {
+    return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(modulus)).intValue();
+  }
+
+  int addMod(int x, int y) {
+    return mod(x + y);
+  }
+
+  int multiplyMod(int x, int y) {
+    return mod((long) x * y);
+  }
+
+  int divideMod(int x, int y) {
+    return multiplyMod(x, modInv(y));
+  }
+
+  int powMod(int base, long exponent) {
+    if (exponent == 0) {
+      return 1;
+    }
+
+    return multiplyMod(
+        (exponent % 2 == 0) ? 1 : base, powMod(multiplyMod(base, base), exponent / 2));
+  }
+}
