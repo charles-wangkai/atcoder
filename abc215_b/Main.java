220754c8e7e379f4b9e8b3c0609c97dbab098dc1
Kai Wang <charles.wangkai@gmail.com>
2026-03-25 11:12:13 +0800

    log2(N)

---
+++ abc215_b/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    long N = sc.nextLong();
+
+    System.out.println(solve(N));
+
+    sc.close();
+  }
+
+  static long solve(long N) {
+    return Long.numberOfTrailingZeros(Long.highestOneBit(N));
+  }
+}
