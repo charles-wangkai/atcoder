037104d0fee95bb94c4e1c5af4793288c63a70a9
Kai Wang <charles.wangkai@gmail.com>
2026-03-27 11:09:19 +0800

    Couples

---
+++ abc359_b/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] A = new int[2 * N];
+    for (int i = 0; i < A.length; ++i) {
+      A[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(A));
+
+    sc.close();
+  }
+
+  static int solve(int[] A) {
+    return (int) IntStream.range(0, A.length - 2).filter(i -> A[i] == A[i + 2]).count();
+  }
+}
