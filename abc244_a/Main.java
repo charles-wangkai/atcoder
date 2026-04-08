12bac6e9babee4a879adc9b1dfc79b6d118d7494
Kai Wang <charles.wangkai@gmail.com>
2026-04-08 11:00:49 +0800

    Last Letter

---
+++ abc244_a/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    sc.nextInt();
+    String S = sc.next();
+
+    System.out.println(solve(S));
+
+    sc.close();
+  }
+
+  static char solve(String S) {
+    return S.charAt(S.length() - 1);
+  }
+}
