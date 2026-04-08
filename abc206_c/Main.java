b3a189cb1d5e6a5b5cf172ff7a78436e044011f0
Kai Wang <charles.wangkai@gmail.com>
2026-04-08 14:21:47 +0800

    Swappable

---
+++ abc206_c/Main.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] A = new int[N];
+    for (int i = 0; i < A.length; ++i) {
+      A[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(A));
+
+    sc.close();
+  }
+
+  static long solve(int[] A) {
+    Map<Integer, Integer> valueToCount = new HashMap<>();
+    for (int Ai : A) {
+      valueToCount.put(Ai, valueToCount.getOrDefault(Ai, 0) + 1);
+    }
+
+    return valueToCount.values().stream()
+            .mapToLong(count -> (long) count * (A.length - count))
+            .sum()
+        / 2;
+  }
+}
