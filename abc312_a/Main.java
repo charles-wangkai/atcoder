a174c0da28bceef45da21b81310cd6aa1c931e59
Kai Wang <charles.wangkai@gmail.com>
2026-04-08 15:22:38 +0800

    Chord

---
+++ abc312_a/Main.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String S = sc.next();
+
+    System.out.println(solve(S) ? "Yes" : "No");
+
+    sc.close();
+  }
+
+  static boolean solve(String S) {
+    return List.of("ACE", "BDF", "CEG", "DFA", "EGB", "FAC", "GBD").contains(S);
+  }
+}
