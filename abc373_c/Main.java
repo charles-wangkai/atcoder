e3fa90dc81367e184c6ab6e2b65b09b4cfa572ac
Kai Wang <charles.wangkai@gmail.com>
2026-04-10 13:17:39 +0800

    Max Ai+Bj

---
+++ abc373_c/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] A = new int[N];
+    for (int i = 0; i < A.length; ++i) {
+      A[i] = sc.nextInt();
+    }
+    int[] B = new int[N];
+    for (int i = 0; i < B.length; ++i) {
+      B[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(A, B));
+
+    sc.close();
+  }
+
+  static int solve(int[] A, int[] B) {
+    return Arrays.stream(A).max().getAsInt() + Arrays.stream(B).max().getAsInt();
+  }
+}
