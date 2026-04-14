b0e0d796e2676dd4bf7b473aa39b6fb6b85f4a82
Kai Wang <charles.wangkai@gmail.com>
2026-04-14 13:40:38 +0800

    Lacked Number

---
+++ abc248_a/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String S = sc.next();
+
+    System.out.println(solve(S));
+
+    sc.close();
+  }
+
+  static int solve(String S) {
+    return IntStream.rangeClosed(0, 9).sum() - S.chars().map(c -> c - '0').sum();
+  }
+}
