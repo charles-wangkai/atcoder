90caeb79517ba52260b2deea6cd8190e6238d905
Kai Wang <charles.wangkai@gmail.com>
2026-04-14 15:08:46 +0800

    Next

---
+++ abc329_b/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] A = new int[N];
+    for (int i = 0; i < A.length; ++i) {
+      A[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(A));
+
+    sc.close();
+  }
+
+  static int solve(int[] A) {
+    int max = Arrays.stream(A).max().getAsInt();
+
+    return Arrays.stream(A).filter(Ai -> Ai != max).max().getAsInt();
+  }
+}
