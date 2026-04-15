b4b26eee420f57b49c980163f40cead24a7cf987
Kai Wang <charles.wangkai@gmail.com>
2026-04-15 15:06:29 +0800

    cat

---
+++ abc394_b/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    String[] S = new String[N];
+    for (int i = 0; i < S.length; ++i) {
+      S[i] = sc.next();
+    }
+
+    System.out.println(solve(S));
+
+    sc.close();
+  }
+
+  static String solve(String[] S) {
+    return Arrays.stream(S)
+        .sorted(Comparator.comparing(String::length))
+        .collect(Collectors.joining());
+  }
+}
