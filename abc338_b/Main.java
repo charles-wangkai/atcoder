4b8d3b6ec06390718cd655b9f65384898ac0620f
Kai Wang <charles.wangkai@gmail.com>
2026-04-16 09:23:27 +0800

    Frequency

---
+++ abc338_b/Main.java
@@ -0,0 +1,30 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String S = sc.next();
+
+    System.out.println(solve(S));
+
+    sc.close();
+  }
+
+  static char solve(String S) {
+    Map<Character, Integer> letterToCount = new HashMap<>();
+    for (char letter : S.toCharArray()) {
+      letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
+    }
+
+    int maxCount = letterToCount.values().stream().mapToInt(Integer::intValue).max().getAsInt();
+
+    return letterToCount.keySet().stream()
+        .filter(letter -> letterToCount.get(letter) == maxCount)
+        .min(Comparator.naturalOrder())
+        .get();
+  }
+}
