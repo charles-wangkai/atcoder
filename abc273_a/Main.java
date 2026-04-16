6171cd6a441419d37bceff3112a51d018c4b25c8
Kai Wang <charles.wangkai@gmail.com>
2026-04-16 09:33:30 +0800

    A Recursive Function

---
+++ abc273_a/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+
+    System.out.println(solve(N));
+
+    sc.close();
+  }
+
+  static int solve(int N) {
+    return IntStream.rangeClosed(1, N).reduce(1, (acc, x) -> acc * x);
+  }
+}
