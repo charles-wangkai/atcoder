a6212bce112af68b5d55ef3479b2462aaa364f6b
Kai Wang <charles.wangkai@gmail.com>
2026-04-16 09:41:30 +0800

    Content Too Large

---
+++ abc413_a/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int M = sc.nextInt();
+    int[] A = new int[N];
+    for (int i = 0; i < A.length; ++i) {
+      A[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(A, M) ? "Yes" : "No");
+
+    sc.close();
+  }
+
+  static boolean solve(int[] A, int M) {
+    return Arrays.stream(A).sum() <= M;
+  }
+}
