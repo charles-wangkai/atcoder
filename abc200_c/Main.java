0a6de294bd802e8af4ac71a469512014f66e440b
Kai Wang <charles.wangkai@gmail.com>
2026-04-18 23:06:38 +0800

    Ringo's Favorite Numbers 2

---
+++ abc200_c/Main.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] A = new int[N];
+    for (int i = 0; i < A.length; ++i) {
+      A[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(A));
+
+    sc.close();
+  }
+
+  static long solve(int[] A) {
+    long result = 0;
+    Map<Integer, Integer> remainderToCount = new HashMap<>();
+    for (int Ai : A) {
+      int remainder = Ai % 200;
+      int count = remainderToCount.getOrDefault(remainder, 0);
+      result += count;
+      remainderToCount.put(remainder, count + 1);
+    }
+
+    return result;
+  }
+}
