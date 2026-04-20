11acb03fb34635f65cd2ed319b9d676aa9bb45fe
Kai Wang <charles.wangkai@gmail.com>
2026-04-20 12:44:45 +0800

    Climbing Takahashi

---
+++ abc235_b/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] H = new int[N];
+    for (int i = 0; i < H.length; ++i) {
+      H[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(H));
+
+    sc.close();
+  }
+
+  static int solve(int[] H) {
+    return IntStream.range(0, H.length)
+        .filter(i -> i == H.length - 1 || H[i] >= H[i + 1])
+        .map(i -> H[i])
+        .findFirst()
+        .getAsInt();
+  }
+}
