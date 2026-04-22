0a3a97b114d4cc58b68a4cbd7be67fff718afb7a
Kai Wang <charles.wangkai@gmail.com>
2026-04-22 15:41:14 +0800

    Echo

---
+++ abc306_a/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    sc.nextInt();
+    String S = sc.next();
+
+    System.out.println(solve(S));
+
+    sc.close();
+  }
+
+  static String solve(String S) {
+    return S.chars().mapToObj(c -> "%c%c".formatted(c, c)).collect(Collectors.joining());
+  }
+}
