8bee9d53d80f6ae80653244bb116270d450db30e
Kai Wang <charles.wangkai@gmail.com>
2026-04-23 17:07:40 +0800

    MissingNo.

---
+++ abc317_b/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] A = new int[N];
+    for (int i = 0; i < A.length; ++i) {
+      A[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(A));
+
+    sc.close();
+  }
+
+  static int solve(int[] A) {
+    return IntStream.rangeClosed(
+                Arrays.stream(A).min().getAsInt(), Arrays.stream(A).max().getAsInt())
+            .sum()
+        - Arrays.stream(A).sum();
+  }
+}
