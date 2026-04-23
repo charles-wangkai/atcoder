d25a845520f8a40dd6f55620ad051b918dc278de
Kai Wang <charles.wangkai@gmail.com>
2026-04-23 18:17:15 +0800

    Standing On The Shoulders

---
+++ abc352_c/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] A = new int[N];
+    int[] B = new int[N];
+    for (int i = 0; i < N; ++i) {
+      A[i] = sc.nextInt();
+      B[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(A, B));
+
+    sc.close();
+  }
+
+  static long solve(int[] A, int[] B) {
+    return Arrays.stream(A).asLongStream().sum()
+        + IntStream.range(0, A.length).map(i -> B[i] - A[i]).max().getAsInt();
+  }
+}
