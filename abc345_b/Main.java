b71a715f5403d2d6e7f5c4b6572553ba3aff1874
Kai Wang <charles.wangkai@gmail.com>
2026-04-23 18:21:19 +0800

    Integer Division Returns

---
+++ abc345_b/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    long X = sc.nextLong();
+
+    System.out.println(solve(X));
+
+    sc.close();
+  }
+
+  static long solve(long X) {
+    return Math.ceilDiv(X, 10);
+  }
+}
