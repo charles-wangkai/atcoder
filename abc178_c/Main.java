1c16b0790df1a71e8bd1f4eb258667520dc05362
Kai Wang <charles.wangkai@gmail.com>
2026-04-27 13:44:06 +0800

    Ubiquity

---
+++ abc178_c/Main.java
@@ -0,0 +1,59 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+  static final ModInt MOD_INT = new ModInt(1_000_000_007);
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+
+    System.out.println(solve(N));
+
+    sc.close();
+  }
+
+  static int solve(int N) {
+    return MOD_INT.addMod(
+        MOD_INT.addMod(MOD_INT.powMod(10, N), -MOD_INT.multiplyMod(MOD_INT.powMod(9, N), 2)),
+        MOD_INT.powMod(8, N));
+  }
+}
+
+class ModInt {
+  int modulus;
+
+  ModInt(int modulus) {
+    this.modulus = modulus;
+  }
+
+  int mod(long x) {
+    return Math.floorMod(x, modulus);
+  }
+
+  int modInv(int x) {
+    return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(modulus)).intValue();
+  }
+
+  int addMod(int x, int y) {
+    return mod(x + y);
+  }
+
+  int multiplyMod(int x, int y) {
+    return mod((long) x * y);
+  }
+
+  int divideMod(int x, int y) {
+    return multiplyMod(x, modInv(y));
+  }
+
+  int powMod(int base, long exponent) {
+    if (exponent == 0) {
+      return 1;
+    }
+
+    return multiplyMod(
+        (exponent % 2 == 0) ? 1 : base, powMod(multiplyMod(base, base), exponent / 2));
+  }
+}
