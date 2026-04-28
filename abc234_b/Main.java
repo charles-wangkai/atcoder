642f050b842d5cf300de998310778c400a61598e
Kai Wang <charles.wangkai@gmail.com>
2026-04-28 09:10:09 +0800

    Longest Segment

---
+++ abc234_b/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] x = new int[N];
+    int[] y = new int[N];
+    for (int i = 0; i < N; ++i) {
+      x[i] = sc.nextInt();
+      y[i] = sc.nextInt();
+    }
+
+    System.out.println("%.9f".formatted(solve(x, y)));
+
+    sc.close();
+  }
+
+  static double solve(int[] x, int[] y) {
+    double result = -1;
+    for (int i = 0; i < x.length; ++i) {
+      for (int j = i + 1; j < x.length; ++j) {
+        result =
+            Math.max(
+                result, Math.sqrt((x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j])));
+      }
+    }
+
+    return result;
+  }
+}
