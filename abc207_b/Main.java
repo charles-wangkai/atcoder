51c351c5be04693d78e2a91153af20299f60dce0
Kai Wang <charles.wangkai@gmail.com>
2026-05-07 16:46:08 +0800

    Hydrate

---
+++ abc207_b/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+    int C = sc.nextInt();
+    int D = sc.nextInt();
+
+    System.out.println(solve(A, B, C, D));
+
+    sc.close();
+  }
+
+  static int solve(int A, int B, int C, int D) {
+    long factor = (long) C * D - B;
+
+    return (factor <= 0) ? -1 : (int) Math.ceilDiv(A, factor);
+  }
+}
