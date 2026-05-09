21313d3e603f954a4dae99c976529248addc15d3
Kai Wang <charles.wangkai@gmail.com>
2026-05-09 14:37:28 +0800

    Unlucky 7

---
+++ abc186_c/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+
+    System.out.println(solve(N));
+
+    sc.close();
+  }
+
+  static int solve(int N) {
+    return (int)
+        IntStream.rangeClosed(1, N)
+            .filter(
+                x -> !Integer.toString(x).contains("7") && !Integer.toOctalString(x).contains("7"))
+            .count();
+  }
+}
