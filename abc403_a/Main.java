856c13e28c864e15bde915446c321df19a5d30ea
Kai Wang <charles.wangkai@gmail.com>
2026-06-06 18:35:05 +0800

    Odd Position Sum

---
+++ abc403_a/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] A = new int[N];
+    for (int i = 0; i < A.length; ++i) {
+      A[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(A));
+
+    sc.close();
+  }
+
+  static int solve(int[] A) {
+    return IntStream.range(0, A.length).filter(i -> i % 2 == 0).map(i -> A[i]).sum();
+  }
+}
