d2965f72fe805a156563beb3b1a30101f23efab3
Kai Wang <charles.wangkai@gmail.com>
2026-06-12 14:15:54 +0800

    Setsubun

---
+++ abc443_b/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int K = sc.nextInt();
+
+    System.out.println(solve(N, K));
+
+    sc.close();
+  }
+
+  static int solve(int N, int K) {
+    int eaten = 0;
+    for (int i = 0; ; ++i) {
+      eaten += N + i;
+      if (eaten >= K) {
+        return i;
+      }
+    }
+  }
+}
