024912e4bb56613a9c78f51aca266ed6e4eada0c
Kai Wang <charles.wangkai@gmail.com>
2026-06-12 10:40:20 +0800

    Job Interview

---
+++ abc298_a/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    sc.nextInt();
+    String S = sc.next();
+
+    System.out.println(solve(S) ? "Yes" : "No");
+
+    sc.close();
+  }
+
+  static boolean solve(String S) {
+    return S.contains("o") && !S.contains("x");
+  }
+}
