1db177137656fa73be2b54e571047198f77b2840
Kai Wang <charles.wangkai@gmail.com>
2026-06-18 15:06:43 +0800

    Closed interval

---
+++ abc454_a/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int L = sc.nextInt();
+    int R = sc.nextInt();
+
+    System.out.println(solve(L, R));
+
+    sc.close();
+  }
+
+  static int solve(int L, int R) {
+    return R - L + 1;
+  }
+}
