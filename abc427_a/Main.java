7712a25909ca48d7ce42f8e585364aac90f2401f
Kai Wang <charles.wangkai@gmail.com>
2026-06-26 17:17:03 +0800

    ABC -> AC

---
+++ abc427_a/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String S = sc.next();
+
+    System.out.println(solve(S));
+
+    sc.close();
+  }
+
+  static String solve(String S) {
+    int index = S.length() / 2;
+
+    return S.substring(0, index) + S.substring(index + 1);
+  }
+}
