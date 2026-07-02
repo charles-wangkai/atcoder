ab7750a4010e6994d261a3f92ee2b310481cd000
Kai Wang <charles.wangkai@gmail.com>
2026-07-02 15:51:13 +0800

    Count Connected Components

---
+++ abc284_c/Main.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int M = sc.nextInt();
+    int[] u = new int[M];
+    int[] v = new int[M];
+    for (int i = 0; i < M; ++i) {
+      u[i] = sc.nextInt();
+      v[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(N, u, v));
+
+    sc.close();
+  }
+
+  static int solve(int N, int[] u, int[] v) {
+    Dsu dsu = new Dsu(N);
+    for (int i = 0; i < u.length; ++i) {
+      dsu.union(u[i] - 1, v[i] - 1);
+    }
+
+    return dsu.buildLeaderToGroup().size();
+  }
+}
+
+class Dsu {
+  int[] parentOrSizes;
+
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
+  }
+
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
+    }
+
+    parentOrSizes[a] = find(parentOrSizes[a]);
+
+    return parentOrSizes[a];
+  }
+
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
+    }
+  }
+
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
+
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
+    }
+
+    return leaderToGroup;
+  }
+}
