b742be859ad771c03b4c2001d6f6873215096776
Kai Wang <charles.wangkai@gmail.com>
2026-07-03 19:28:19 +0800

    Secret Numbers

---
+++ abc462_a/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String S = sc.next();
+
+    System.out.println(solve(S));
+
+    sc.close();
+  }
+
+  static String solve(String S) {
+    return S.chars()
+        .filter(Character::isDigit)
+        .mapToObj(c -> (char) c)
+        .map(String::valueOf)
+        .collect(Collectors.joining());
+  }
+}
