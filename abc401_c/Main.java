eb64bf2590ecf5555d4b4960ad7e0372c3377c45
Kai Wang <charles.wangkai@gmail.com>
2026-07-03 11:06:37 +0800

    K-bonacci

---
+++ abc401_c/Main.java
@@ -0,0 +1,71 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+  static final ModInt MOD_INT = new ModInt(1_000_000_000);
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int K = sc.nextInt();
+
+    System.out.println(solve(N, K));
+
+    sc.close();
+  }
+
+  static int solve(int N, int K) {
+    int[] A = new int[N + 1];
+    int sum = 0;
+    for (int i = 0; i < A.length; ++i) {
+      if (i < K) {
+        A[i] = 1;
+      } else {
+        A[i] = sum;
+        sum = MOD_INT.addMod(sum, -A[i - K]);
+      }
+
+      sum = MOD_INT.addMod(sum, A[i]);
+    }
+
+    return A[N];
+  }
+}
+
+class ModInt {
+  int modulus;
+
+  ModInt(int modulus) {
+    this.modulus = modulus;
+  }
+
+  int mod(long x) {
+    return Math.floorMod(x, modulus);
+  }
+
+  int modInv(int x) {
+    return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(modulus)).intValue();
+  }
+
+  int addMod(int x, int y) {
+    return mod(x + y);
+  }
+
+  int multiplyMod(int x, int y) {
+    return mod((long) x * y);
+  }
+
+  int divideMod(int x, int y) {
+    return multiplyMod(x, modInv(y));
+  }
+
+  int powMod(int base, long exponent) {
+    if (exponent == 0) {
+      return 1;
+    }
+
+    return multiplyMod(
+        (exponent % 2 == 0) ? 1 : base, powMod(multiplyMod(base, base), exponent / 2));
+  }
+}
