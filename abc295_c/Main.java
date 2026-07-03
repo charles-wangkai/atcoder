04fcc796e4c5c8b2e6246d2b3155d6d90ffc7766
Kai Wang <charles.wangkai@gmail.com>
2026-07-03 09:42:48 +0800

    Socks

---
+++ abc295_c/Main.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] A = new int[N];
+    for (int i = 0; i < A.length; ++i) {
+      A[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(A));
+
+    sc.close();
+  }
+
+  static int solve(int[] A) {
+    Map<Integer, Integer> valueToCount = new HashMap<>();
+    for (int Ai : A) {
+      valueToCount.put(Ai, valueToCount.getOrDefault(Ai, 0) + 1);
+    }
+
+    return valueToCount.values().stream().mapToInt(count -> count / 2).sum();
+  }
+}
