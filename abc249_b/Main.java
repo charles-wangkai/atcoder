217797449ed9d011c0ca6f6c72dbc82774af4a4f
Kai Wang <charles.wangkai@gmail.com>
2026-07-07 14:54:12 +0800

    Perfect String

---
+++ abc249_b/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String S = sc.next();
+
+    System.out.println(solve(S) ? "Yes" : "No");
+
+    sc.close();
+  }
+
+  static boolean solve(String S) {
+    return S.chars().anyMatch(Character::isUpperCase)
+        && S.chars().anyMatch(Character::isLowerCase)
+        && S.chars().distinct().count() == S.length();
+  }
+}
