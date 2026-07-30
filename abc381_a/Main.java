093427585626ac9ea4dd6e31749b7b679b1040a0
Kai Wang <charles.wangkai@gmail.com>
2026-07-30 09:53:25 +0800

    11/22 String

---
+++ abc381_a/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    sc.nextInt();
+    String S = sc.next();
+
+    System.out.println(solve(S) ? "Yes" : "No");
+
+    sc.close();
+  }
+
+  static boolean solve(String S) {
+    return S.length() % 2 == 1
+        && S.equals("1".repeat(S.length() / 2) + "/" + "2".repeat(S.length() / 2));
+  }
+}
