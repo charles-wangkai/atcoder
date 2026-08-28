499dd2c07a8358627d9d626b19e56fb496861627
Kai Wang <charles.wangkai@gmail.com>
2026-08-28 17:05:01 +0800

    Snuke the Cookie Picker

---
+++ abc305_c/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int H = sc.nextInt();
+    int W = sc.nextInt();
+    char[][] grid = new char[H][W];
+    for (int r = 0; r < H; ++r) {
+      String line = sc.next();
+      for (int c = 0; c < W; ++c) {
+        grid[r][c] = line.charAt(c);
+      }
+    }
+
+    System.out.println(solve(grid));
+
+    sc.close();
+  }
+
+  static String solve(char[][] grid) {
+    int H = grid.length;
+    int W = grid[0].length;
+
+    int minR = Integer.MAX_VALUE;
+    int maxR = Integer.MIN_VALUE;
+    int minC = Integer.MAX_VALUE;
+    int maxC = Integer.MIN_VALUE;
+    for (int r = 0; r < H; ++r) {
+      for (int c = 0; c < W; ++c) {
+        if (grid[r][c] == '#') {
+          minR = Math.min(minR, r);
+          maxR = Math.max(maxR, r);
+          minC = Math.min(minC, c);
+          maxC = Math.max(maxC, c);
+        }
+      }
+    }
+
+    for (int r = minR; ; ++r) {
+      for (int c = minC; c <= maxC; ++c) {
+        if (grid[r][c] == '.') {
+          return "%d %d".formatted(r + 1, c + 1);
+        }
+      }
+    }
+  }
+}
