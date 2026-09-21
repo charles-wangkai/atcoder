ae5fad2e215118e27db7b95ff07e7ffe301fda81
Kai Wang <charles.wangkai@gmail.com>
2026-09-21 11:25:06 +0800

    Standings

---
+++ abc308_c/Main.java
@@ -0,0 +1,37 @@
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] A = new int[N];
+    int[] B = new int[N];
+    for (int i = 0; i < N; ++i) {
+      A[i] = sc.nextInt();
+      B[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(A, B));
+
+    sc.close();
+  }
+
+  static String solve(int[] A, int[] B) {
+    return IntStream.range(0, A.length)
+        .boxed()
+        .sorted(
+            ((Comparator<Integer>)
+                    (i1, i2) ->
+                        Long.compare(
+                            (long) A[i1] * (A[i2] + B[i2]), (long) A[i2] * (A[i1] + B[i1])))
+                .reversed()
+                .thenComparing(i -> i))
+        .map(i -> i + 1)
+        .map(String::valueOf)
+        .collect(Collectors.joining(" "));
+  }
+}
