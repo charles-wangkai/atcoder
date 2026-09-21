9c34c05ba5c9ef434fcd34f21e5273af36a8f63e
Kai Wang <charles.wangkai@gmail.com>
2026-09-21 18:33:33 +0800

    Redistribution

---
+++ abc178_d/Main.java
@@ -0,0 +1,66 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+  static final ModInt MOD_INT = new ModInt(1_000_000_007);
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int S = sc.nextInt();
+
+    System.out.println(solve(S));
+
+    sc.close();
+  }
+
+  static int solve(int S) {
+    int[] dp = new int[S + 1];
+    dp[0] = 1;
+
+    for (int i = 1; i < dp.length; ++i) {
+      for (int j = i - 3; j >= 0; --j) {
+        dp[i] = MOD_INT.addMod(dp[i], dp[j]);
+      }
+    }
+
+    return dp[dp.length - 1];
+  }
+}
+
+class ModInt {
+  int modulus;
+
+  ModInt(int modulus) {
+    this.modulus = modulus;
+  }
+
+  int mod(long x) {
+    return Math.floorMod(x, modulus);
+  }
+
+  int modInv(int x) {
+    return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(modulus)).intValue();
+  }
+
+  int addMod(int x, int y) {
+    return mod(x + y);
+  }
+
+  int multiplyMod(int x, int y) {
+    return mod((long) x * y);
+  }
+
+  int divideMod(int x, int y) {
+    return multiplyMod(x, modInv(y));
+  }
+
+  int powMod(int base, long exponent) {
+    if (exponent == 0) {
+      return 1;
+    }
+
+    return multiplyMod(
+        (exponent % 2 == 0) ? 1 : base, powMod(multiplyMod(base, base), exponent / 2));
+  }
+}
