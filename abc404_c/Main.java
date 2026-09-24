228096ef273a83a1bc53e9903fb317307d28ec83
Kai Wang <charles.wangkai@gmail.com>
2026-09-24 17:08:10 +0800

    Cycle Graph?

---
+++ abc404_c/Main.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int M = sc.nextInt();
+    int[] A = new int[M];
+    int[] B = new int[M];
+    for (int i = 0; i < M; ++i) {
+      A[i] = sc.nextInt();
+      B[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(N, A, B) ? "Yes" : "No");
+
+    sc.close();
+  }
+
+  static boolean solve(int N, int[] A, int[] B) {
+    int[] degrees = new int[N];
+    Dsu dsu = new Dsu(N);
+    for (int i = 0; i < A.length; ++i) {
+      ++degrees[A[i] - 1];
+      ++degrees[B[i] - 1];
+
+      dsu.union(A[i] - 1, B[i] - 1);
+    }
+
+    return Arrays.stream(degrees).allMatch(degree -> degree == 2)
+        && dsu.buildLeaderToGroup().size() == 1;
+  }
+}
+
+class Dsu {
+  int[] parentOrSizes;
+
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
+  }
+
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
+    }
+
+    parentOrSizes[a] = find(parentOrSizes[a]);
+
+    return parentOrSizes[a];
+  }
+
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
+    }
+  }
+
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
+
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
+    }
+
+    return leaderToGroup;
+  }
+}
